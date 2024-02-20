0c8d0a126dc79fe968fb622d8eee61a64919e8fe
Andy Phan <AndyyyPhan@gmail.com>
2024-02-20 15:04:51 -0500

    Updated settings.py to have user be directed to 'blog-home' after logging in and 'login' after going to user profile page

--- InstaGym/settings.py
+++ InstaGym/settings.py
@@ -130,6 +130,9 @@ CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
